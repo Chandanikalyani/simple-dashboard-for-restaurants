9dcb6c757b2b5c0023a1be9eb82bb00c43dd33f7
Chandanikalyani <chandanikalyani9@gmail.com>
2024-04-28 23:28:37 +0530

    #	new file:   src/Assets/map.jpeg #	new file:   src/Assets/map.png #	modified:   src/Components/Body/Body.jsx #	modified:   src/Components/Container.jsx #	modified:   src/Components/SideMenu/SideMenu.jsx

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { AiOutlineSearch } from "react-icons/ai";
 
 const Body = () => {
   return (
-    <div>
-      <h1>Body</h1>
+    <div className='bg-bodyBg h-[100%] basis-80 p-8'>
+    {/* Top Section */}
+    <div className='flex items-center border-b-2 pd-2 basis-1/2 gap-2'>
+      <AiOutlineSearch className='text-hoverColor text-[20px] cursor-pointer'/>
+      <input type="text" placeholder='search your favourite' className='boder-none outline-none placeholder:text-sm'/>
+
+
+    </div>
+ 
     </div>
   )
 }
--- src/Components/SideMenu/SideMenu.jsx
+++ src/Components/SideMenu/SideMenu.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 import { GiHamburger } from 'react-icons/gi';
 
 import { AiFillHome } from "react-icons/ai";
+import { BiRestaurant } from "react-icons/bi";
+import { BsBookmarkCheckFill } from "react-icons/bs";
+import { AiFillHeart } from "react-icons/ai";
 
+// Map Image==>
+ import mapImage  from '../../Assets/map.png'
+ import adminImage from '../../Assets/User.png'
+
+const User="Chandhi";
 const SideMenu = () => {
   return (
     <div className='bg-SideMenuBg relative h-[100%] basic-80 p-4'>
@@ -16,16 +24,38 @@ const SideMenu = () => {
       {/* User Div */}
       <div className='flex items-center justify-center text-[white] flex-col mt-[3rem] admin'>
 
-        <img src=""alt="Admin Image" className='h-[80px] w-[80px] boder-red-200 boder-[40px] object-cover rounded-full'/>
+        <img src={adminImage} alt="Admin Image" className='h-[80px] w-[80px] boder-red-200 boder-[40px] object-cover rounded-full'/>
         <span className='opacity-70 mt-2 text-gray-400'>Welcome</span>
-        <h3 className='font-bold text-textColor'></h3>
+        <h3 className='font-bold text-textColor'>{User}</h3>
       </div>
 
       {/* simple Menu */}
       <div className='m-auto grid justify-center mt-4'>
         <li className='flex pt-3 pb-3 pl-3 gap-2 hover:opacity-100 items-center'>
-          <AiFillHome/>
+          <AiFillHome className='text-white'/>
+          <span className='text-textColor'>Home</span>
         </li>
+
+        <li className='flex pt-3 pb-3 pl-3 gap-2 hover:opacity-100 items-center'>
+          <BiRestaurant className='text-white'/>
+          <span className='text-textColor'>Categories</span>
+        </li>
+
+        <li className='flex pt-3 pb-3 pl-3 gap-2 hover:opacity-100 items-center'>
+          <BsBookmarkCheckFill className='text-white'/>
+          <span className='text-textColor'>Reservations</span>
+        </li>
+
+        <li className='flex pt-3 pb-3 pl-3 gap-2 hover:opacity-100 items-center'>
+          <AiFillHeart className='text-white'/>
+          <span className='text-textColor'>Favourites</span>
+        </li>
+      </div>
+
+      {/* sidemenu Map Div */}
+      <div className='mt-5 h-[14rem] w-full rounded-xl overflow-hidden'>
+        <img src={mapImage} alt="Map Image" className='object-cover h-[75%] w-[100%]'/>
+
       </div>
     </div>
   )
